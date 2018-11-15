5ed836fa81bb487bc2fb880990cf1fd5bf90994b
Damian Michalski <damian-michalski@tlen.pl>
2018-11-15 07:41:19 +0100

    oop - oop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, discount) {
     this.brand = brand;
     this.price = price;
+    this.discount = discount;
     this.color = color;
 }
 
@@ -8,10 +9,14 @@ Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var SamsungGalaxyS6 = new Phone("Samsung Galaxy S6", 1250, "gold");
-var OnePlusOne = new Phone("OnePlus One", 250, "brown");
+var iPhone6S = new Phone("Apple", 2250, 100, "silver");
+var samsungGalaxyS6 = new Phone("Samsung Galaxy S6", 1250, 100, "gold");
+var onePlusOne = new Phone("OnePlus One", 250, 100, "brown");
 
 iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
+
+Phone.prototype.applyDiscount = function() {
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the discounted price is " + this.price - this.discount + ".");
+}
