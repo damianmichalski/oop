7b9f9019309fe27beea81b7016b3ccbaceb55a71
DamianM <damian.michalski@kramp.com>
2018-11-15 12:42:38 +0100

    oop - oop

--- script.js
+++ script.js
@@ -2,18 +2,10 @@ function Phone(brand, price, color, discount) {
     this.brand = brand;
     this.price = price;
     this.color = color;
-    this.discount = discount;
-    // this.discount = function() {
-    //     this.discount = this.price - 100;
-    // };
 }
 
 Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
-
-Phone.prototype.applyDiscount  = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the discounted price is " + (this.price - this.discount) + ".");
 }
 
 var iPhone6S = new Phone("Apple", 2250, "silver");
@@ -24,11 +16,12 @@ iPhone6S.printInfo();
 samsungGalaxyS6.printInfo();
 onePlusOne.printInfo();
 
-var iPhone6SNew = new Phone("Apple", 2250, "silver", 100);
-var samsungGalaxyS6New = new Phone("Samsung Galaxy S6", 1250, "gold", 100);
-var onePlusOne = new Phone("OnePlus One", 250, "brown", 100);
+Phone.prototype.applyDiscount  = function(discount) {
+    this.discount = discount;
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the discounted price is " + (this.price - this.discount) + ".");
+}
 
-iPhone6SNew.applyDiscount();
-samsungGalaxyS6New.applyDiscount();
-onePlusOne.applyDiscount();
+iPhone6S.applyDiscount(100);
+samsungGalaxyS6.applyDiscount(100);
+onePlusOne.applyDiscount(100);
 
