c908f2f7170a890326fa1394813cbbd276e19399
Damian Michalski <damian-michalski@tlen.pl>
2018-11-15 07:34:34 +0100

    oop - kanban

--- script.js
+++ script.js
@@ -1,17 +1,96 @@
-function Phone(brand, price, color) {
-    this.brand = brand;
-    this.price = price;
-    this.color = color;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    function randomString() {
+        var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+        var str = '';
+        for (var i = 0; i < 10; i++) {
+            str += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return str;
+    }
+    function generateTemplate(name, data, basicElement) {
+        var template = document.getElementById(name).innerHTML;
+        var element = document.createElement(basicElement || 'div');
 
-Phone.prototype.printInfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
+        Mustache.parse(template);
+        element.innerHTML = Mustache.render(template, data);
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var SamsungGalaxyS6 = new Phone("Samsung Galaxy S6", 1250, "gold");
-var OnePlusOne = new Phone("OnePlus One", 250, "brown");
+        return element;
+    }
+    function Column(name) {
+        var self = this;
 
-iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
+        this.id = randomString();
+        this.name = name;
+        this.element = generateTemplate('column-template', { name: this.name, id: this.id });
+
+        this.element.querySelector('.column').addEventListener('click', function (event) {
+            if (event.target.classList.contains('btn-delete')) {
+                self.removeColumn();
+            }
+
+            if (event.target.classList.contains('add-card')) {
+                self.addCard(new Card(prompt("Enter the name of the card")));
+            }
+        });
+    }
+    Column.prototype = {
+        addCard: function(card) {
+            this.element.querySelector('ul').appendChild(card.element);
+        },
+        removeColumn: function() {
+            this.element.parentNode.removeChild(this.element);
+        }
+    };
+    function Card(description) {
+        var self = this;
+
+        this.id = randomString();
+        this.description = description;
+        this.element = generateTemplate('card-template', { description: this.description }, 'li');
+
+        this.element.querySelector('.card').addEventListener('click', function (event) {
+            event.stopPropagation();
+
+            if (event.target.classList.contains('btn-delete')) {
+                self.removeCard();
+            }
+        });
+    }
+    Card.prototype = {
+        removeCard: function() {
+            this.element.parentNode.removeChild(this.element);
+        }
+    }
+    var board = {
+        name: 'Kanban Board',
+        addColumn: function(column) {
+            this.element.appendChild(column.element);
+            initSortable(column.id); //About this feature we will tell later
+        },
+        element: document.querySelector('#board .column-container')
+    };
+    function initSortable(id) {
+        var el = document.getElementById(id);
+        var sortable = Sortable.create(el, {
+            group: 'kanban',
+            sort: true
+        });
+    }
+    // CREATING COLUMNS
+    var todoColumn = new Column('To do');
+    var doingColumn = new Column('Doing');
+    var doneColumn = new Column('Done');
+
+// ADDING COLUMNS TO THE BOARD
+    board.addColumn(todoColumn);
+    board.addColumn(doingColumn);
+    board.addColumn(doneColumn);
+
+// CREATING CARDS
+    var card1 = new Card('New task');
+    var card2 = new Card('Create kanban boards');
+
+// ADDING CARDS TO COLUMNS
+    todoColumn.addCard(card1);
+    doingColumn.addCard(card2);
+});
